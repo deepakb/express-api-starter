0347a49ef68767aa0811ca9eaa1b9ea2be6aaa57
Deepak Biswal <deepak.biswal@cantire.com>
2024-04-23 22:33:14 +0530

    Fix chat issue

--- src/services/github.sevice.ts
+++ src/services/github.sevice.ts
@@ -40,7 +40,7 @@ export class GithubService {
       const retriever = await vectorStore.asRetriever({
         searchType: 'mmr',
         searchKwargs: {
-          fetchK: 20,
+          fetchK: 100,
           lambda: 0.1
         }
       });
@@ -54,17 +54,17 @@ export class GithubService {
       });
       const questionGeneratorTemplate = ChatPromptTemplate.fromMessages([
         AIMessagePromptTemplate.fromTemplate(
-          'Given the following conversation about a codebase and a follow up question, rephrase the follow up question to be a standalone question.'
+          "Given a GitHub repository description and a user's question about the code, rephrase the user's question to be a standalone question suitable for searching the codebase."
         ),
         new MessagesPlaceholder('chat_history'),
         AIMessagePromptTemplate.fromTemplate(`
-          Follow Up Input: {question}
-          Standalone question:
+          User Question: {question}\n
+          Standalone Question for Code Search:\n
         `)
       ]);
       const combineDocumentsPrompt = ChatPromptTemplate.fromMessages([
         AIMessagePromptTemplate.fromTemplate(
-          "Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.\n\n{context}\n\n"
+          'The provided GitHub repository code is embedded in a vector space. Use the following pieces of context to answer the question at the end. Give answer valid to the respository codebase only and specific to it.\n\n{context}\n\n'
         ),
         new MessagesPlaceholder('chat_history'),
         HumanMessagePromptTemplate.fromTemplate('Question: {question}')
@@ -78,6 +78,7 @@ export class GithubService {
           },
           context: async (output: string) => {
             const relevantDocs = await retriever.getRelevantDocuments(output);
+            console.log({ relevantDocs });
             return formatDocumentsAsString(relevantDocs);
           }
         },
